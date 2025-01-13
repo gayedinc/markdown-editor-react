bed54bbd846d5a792d4564e505edf42e7c66475d
[Gaye Dinç] <gayedinc190@gmail.com>
2025-01-13 12:15:12 +0300

    markdown editor react projesi yüklendi

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { MarkDownEditor } from './MarkDown.jsx';
+
+const defaultMarkdown = `
+  # Welcome to Markdown
+
+  Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents.
+
+  ## How to use this?
+
+  1. Write markdown in the markdown editor window
+  2. See the rendered markdown in the preview window
+
+  ### Features
+
+  - Create headings, paragraphs, links, blockquotes, inline-code, code blocks, and lists
+  - Name and save the document to access again later
+  - Choose between Light or Dark mode depending on your preference
+
+  > This is an example of a blockquote. If you would like to learn more about markdown syntax, you can visit this [markdown cheatsheet](https://www.markdownguide.org/cheat-sheet/).
+
+  #### Headings
+
+  To create a heading, add the hash sign (#) before the heading. The number of number signs you use should correspond to the heading level. You'll see in this guide that we've used all six heading levels (not necessarily in the correct way you should use headings!) to illustrate how they should look.
+
+  ##### Lists
+
+  You can see examples of ordered and unordered lists above.
+
+  ###### Code Blocks
+
+  This markdown editor allows for inline-code snippets, like this: \`<p>I'm inline</p>\`. It also allows for larger code blocks like this:
+
+  \`\`\`
+  <main>
+    <h1>This is a larger code block</h1>
+  </main>
+  \`\`\`
+`;
+
+function App() {
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
+  const [showPreview, setShowPreview] = useState(false); // önizleme olayı
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // hamburger menü
+  const [documentList, setDocumentList] = useState([
+    { name: "welcome.md", date: "01 April 2022", id: crypto.randomUUID() },
+    { name: "untitled-document.md", date: "26 December 2022", id: crypto.randomUUID() }
+  ]);
+  const [currentDocument, setCurrentDocument] = useState(null);
+
+  return (
+    <>
+      <div className="container" style={{ gridTemplateColumns: isMenuOpen ? "250px 1fr" : "" }}>
+        <MarkDownEditor
+          documentList={documentList}
+          setDocumentList={setDocumentList}
+          currentDocument={currentDocument}
+          setCurrentDocument={setCurrentDocument}
+          setMarkdown={setMarkdown}
+          setShowPreview={setShowPreview}
+          markdown={markdown}
+          showPreview={showPreview}
+          setIsMenuOpen={setIsMenuOpen}
+          isMenuOpen={isMenuOpen} />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/MarkDown.jsx
@@ -0,0 +1,136 @@
+import Markdown from 'marked-react';
+
+export function MarkDownEditor({ setCurrentDocument, currentDocument, documentList, setDocumentList, setMarkdown, showPreview, setShowPreview, markdown, setIsMenuOpen, isMenuOpen }) {
+
+  function togglePreview() {
+    setShowPreview((prevState) => !prevState);
+  }
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
+  function hamburgerMenu() {
+    setIsMenuOpen((prevState) => !prevState);
+  }
+
+  function handleNewDocument() {
+    const newDocument = {
+      name: "untitled-document.md",
+      id: crypto.randomUUID(),
+      date: new Date().toLocaleDateString(),
+    };
+    setDocumentList((prevList) => [...prevList, newDocument]);
+  }
+
+  function openDocument(doc) {
+    console.log(doc);
+    setMarkdown(doc.content || "");
+    setIsMenuOpen(false);
+    setCurrentDocument(doc);
+  }
+
+  function updatedDocument(e) {
+    e.preventDefault(); // Sayfanın yenilenmesini önle
+    const form = new FormData(e.target);
+    const formObj = Object.fromEntries(form);
+    setDocumentList((prevList) =>
+      prevList.map((doc) =>
+        doc.id === currentDocument.id
+          ? { ...doc, ...formObj }
+          : doc
+      )
+    );
+  }
+
+  function deleteDocument(id) {
+    if (id !== null) {
+      const currentList = documentList.filter(x => x.id !== id)
+      setDocumentList(currentList);
+      setMarkdown("");
+    }
+  }
+
+  return (
+    <>
+      <div className="header-modal" style={{ display: isMenuOpen ? "grid" : "none" }}>
+        <div className="hamburger-menu-overlay">
+          <div className="menu-header">
+            <h1>MARKDOWN</h1>
+          </div>
+          <div className="menu-content">
+            <h2>MY DOCUMENTS</h2>
+            <button onClick={handleNewDocument}>+ New Document</button>
+          </div>
+          <div className="document-list">
+            <ul>
+              {documentList.map((doc) => (
+                <li key={doc.id} onClick={() => openDocument(doc)}>
+                  <img src="public/img/file-header-icon.svg" alt="File Icon" />
+                  <div className="document-info">
+                    <span>{doc.date}</span>
+                    <p>{doc.name}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="header">
+        <div className="header-content">
+          <div className="hamburger-menu">
+            <img
+              src={isMenuOpen ? "public/img/hamburger-menu-close.svg" : "public/img/hamburger-menu.svg"}
+              alt="Hamburger Menu"
+              onClick={hamburgerMenu}
+            />
+          </div>
+          <div className="header-inner">
+            <div className="file-content">
+              <img src="public/img/file-header-icon.svg" alt="File Icon" />
+              <p>{currentDocument ? currentDocument.name : "welcome.md"}</p>
+            </div>
+            <div className="file-delete-save">
+              <div className="delete-btn">
+                <img onClick={() => deleteDocument(currentDocument.id)} src="public/img/delete-header-btn.svg" alt="Delete File Icon" />
+              </div>
+              <button type="submit" form="markdownForm" className="save-btn">
+                <img src="public/img/save-header-btn.svg" alt="Save File Icon" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="markdown-container">
+          <form id="markdownForm" onSubmit={updatedDocument}>
+            <div className="markdown-preview-area">
+              <label>{showPreview ? "PREVIEW" : "MARKDOWN"}</label>
+              <img
+                src={showPreview ? "public/img/close-eye-icon.svg" : "public/img/open-eye-icon.svg"}
+                alt=""
+                onClick={togglePreview}
+              />
+            </div>
+            <div className="text-container">
+              {!showPreview ? (
+                <textarea
+                  className="mobile-textarea"
+                  name="content"
+                  onChange={handleChange}
+                  value={markdown}
+                  rows="30"
+                  cols="20"
+                  placeholder="Type some markdown here..."
+                ></textarea>
+              ) : (
+                <div className="preview-container">
+                  <Markdown>{markdown}</Markdown>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
